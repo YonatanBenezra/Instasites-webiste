27f5d05dfa6753ad86036265ad9303d267043174
YonatanBenezra <yonatanbenezra1@gmail.com>
2021-12-05 16:45:37 +0200

    first

---
+++ src/components/footer.jsx
@@ -0,0 +1,21 @@
+export const Footer = (props) => {
+  
+  return (
+    <div>
+      <div id='footer'>
+        <div className='container text-center'>
+          <p>
+            &copy; 2021{' '}
+            <a href='https://www.instagram.com/instasites.io/' rel='nofollow'>
+              Instagram
+            </a> {' '}
+            Feel free {' '}
+            <a href='mailto:instasites.io@gmail.com'>
+            to get in touch
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/header.jsx
@@ -0,0 +1,33 @@
+import ParticlesBg from "particles-bg";
+
+export const Header = (props) => {
+  return (
+    <header id='header'>
+      <div className='intro'>
+        <div className='overlay'>
+          <div className='container'>
+          
+            <div className='row'>
+              <div className='col-md-8 col-md-offset-2 intro-text'>
+                <h1>
+                  {props.data ? props.data.title : 'Loading'}{' '}
+                  <span className="title_2">
+                  {props.data ? props.data.title_2 : 'Loading'}
+                  </span>
+                  <span></span>
+                </h1>
+                <p>{props.data ? props.data.paragraph : 'Loading'}</p>
+                <a
+                  href='#about'
+                  className='btn btn-custom btn-lg page-scroll'
+                >
+                  Learn More
+                </a>{' '}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
